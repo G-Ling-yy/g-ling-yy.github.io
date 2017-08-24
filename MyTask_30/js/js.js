913f6f4374e674a14544c6c722671d8a9a3a3302
G_Ling_yy <934830721@qq.com>
2017-08-25 00:08:25 +0800

    task30 practice postmessage (section)

---
+++ MyTask_30/js/js.js
@@ -0,0 +1,15 @@
+(function(){
+  var myt = document.querySelector('#mytextarea');
+  var myif = document.querySelector('iframe');
+
+  myt.focus();
+
+  myt.addEventListener('keyup',function(e){
+    if (this.value.length === 0) return;
+    if (e.ctrlKey && e.keyCode === 13) {
+      var data = this.value.trim();
+      myif.contentWindow.postMessage(data,'*');
+      this.value = '';
+    }
+  })
+})()
