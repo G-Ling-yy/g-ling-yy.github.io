68208f4dfecffc8c6ddc0c32726ea5e2344efd5f
G_Ling_yy <934830721@qq.com>
2018-04-04 23:31:49 +0800

    mdfy QQ_uid

--- MyResume/js/myinfo.js
+++ MyResume/js/myinfo.js
@@ -10,7 +10,7 @@ class MyInfo {
       <p>性别：<span>${this.obj.sex}</span></p>
       <p>2016年7月毕业于 <span>${this.obj.school}</span></p>
       <p>所习专业：<span>${this.obj.major}</span></p>
-      <p>QQ：<a href="tencent://message/?uin=1145192310">${this.obj.QQ}</a></p>
+      <p>QQ：<a href="tencent://message/?uin=${this.obj.QQ}">${this.obj.QQ}</a></p>
       <p>TEL：<a href="tel:18860913014">${this.obj.tel}</a></p>
       <p>自我评价: <a id="rsrx">${this.obj.pj}</a></p>`
     this.$el.innerHTML = txt
