1d330d5a3af593a13d751f15e6b1d86c96e1e9a3
凌 <934830721@qq.com>
2019-08-04 16:18:38 +0800

    图片路径线上又不对了再改下试试

--- MyResume_v19/resume_v19/src/components/pc_hello_comp.vue
+++ MyResume_v19/resume_v19/src/components/pc_hello_comp.vue
@@ -104,7 +104,7 @@ export default {
 	created() {
 		const myPic = new Image()
 		window.myPic = myPic
-		myPic.src = require('../assets/img/pc_pic.jpg')
+		myPic.src = '../assets/img/pc_pic.jpg'
 		myPic.onload = () => this.imgLoaded = true
 	},
 
