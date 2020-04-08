2aae5571531a792882cd560fbb64d45a3f801a07
凌 <934830721@qq.com>
2020-04-09 00:25:22 +0800

    resume 20 add /dist

---
+++ MyResumeDemo_v20/dist/precache-manifest.3b227a9a429e441e7833f52f28fec482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3ec96d63925d8fcb6c",
+    "url": "assets/css/app.0a106172.css"
+  },
+  {
+    "revision": "28f39e682c14dcc655bf",
+    "url": "assets/js/about.288b0054.js"
+  },
+  {
+    "revision": "8d3ec96d63925d8fcb6c",
+    "url": "assets/js/app.f51c185c.js"
+  },
+  {
+    "revision": "786ccc4b6b1a74a86459",
+    "url": "assets/js/chunk-vendors.6044e0f9.js"
+  },
+  {
+    "revision": "114a2c5706872aefb7d91ca2c8b77490",
+    "url": "index.html"
+  },
+  {
+    "revision": "d49cf6b1baa26ed7d400b042944ed7d0",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "3a0e0033fc68deae3056dfa5d498da60",
+    "url": "userData.json"
+  }
+]);
