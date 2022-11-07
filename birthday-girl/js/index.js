76cb40c3fa982cc2930890c848c8bf23e0e8f73a
ling <934830721@qq.com>
2022-11-07 23:51:46 +0800

    初步结构

--- birthday-girl/js/index.js
+++ birthday-girl/js/index.js
@@ -1,7 +1,24 @@
-const $girl = document.querySelector('.girl')
+const $girl = document.querySelector('.girl'),
+	$btn = document.querySelector('.right-arrow')
 
-$girl.addEventListener('click', e => {
+let right = 0
 
+// 按钮事件
+$btn.addEventListener('click', e => {
+	const act = $girl.classList.contains('walking') ? 'remove' : 'add'
+	$girl.classList[act]('walking')
+
+	// 若 走步
+	if (act === 'add') {
+		$girl.style.right = `${right += 16}%`
+
+		// 隐藏按钮
+		// $btn.style.visibility = 'hidden'
+	}
+})
+
+// 女孩事件
+$girl.addEventListener('transitionend', e => {
 	const act = $girl.classList.contains('walking') ? 'remove' : 'add'
 	$girl.classList[act]('walking')
 })
