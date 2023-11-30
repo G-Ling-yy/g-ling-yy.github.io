4d5d9b796daeb17bfce3e8ac114f3b505f4f3345
ling <934830721@qq.com>
2023-12-01 00:09:45 +0800

    律诗津梁update

---
+++ MyResumeDemo_v23/src/App.vue
@@ -0,0 +1,249 @@
+<script setup>
+  import {ref} from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+
+const isGetData = ref(false),
+user = ref({})
+
+fetch('./userData.json').then(res => res.json()).then(res => {
+      // this.isGetData = true
+      isGetData.value = true
+      console.log('res', res)
+      // 展示demo
+      // this.user = res.demo
+      // 展示user
+      // this.user = res.user
+      user.value = res.user
+    })
+</script>
+
+<template>
+<!--   <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+
+  <div class="home">
+    <header>
+      <!-- 个人简介 -->
+      <div class="texts">
+        <h1>{{isGetData ? user.name || '(user数据为空)' : 'loading···'}}</h1>
+        <div class="profile">
+          <p>
+            <span>男</span>
+            <span>94.11</span>
+            <span>6年</span>
+            <span>Web前端</span>
+          </p>
+          <p>
+            <span>18860913014</span>
+            <span>934830721@qq.com</span>
+          </p>
+        </div>
+      </div>
+
+      <!-- 个人头像 -->
+      <div class="avatar-wrapper">
+        <img v-if="user.avatar" :src="user.avatar" alt="" class="avatar">
+        <div v-else class="default-avatar">
+          <div class="left-eye"></div>
+          <div class="right-eye"></div>
+          <div class="mouth"></div>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <Block v-for="(block, index) in user.blocks" :key="index" :blockValue="block"/>
+    </main>
+
+    <footer v-show="isGetData && user.name">
+      感谢您花时间阅读我的简历，期待能有机会与优秀的您共事。
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+  @media print {
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+
+    // .block-comp {
+    //   &:first-of-type {
+
+    //   }
+      
+    // }
+  }
+}
+
+  @page {
+    margin: 0;
+    padding: 0;
+    page-orientation: upright;
+    size: 210mm 297mm;
+  }
+</style>
+
+<style lang="scss" scoped>
+  .home {
+    header {
+      background-color: var(--themeColor);
+      display: flex;
+      height: 13rem;
+      padding: 3rem 5% 0;
+      position: relative;
+
+      .texts {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding-top: 1.8rem;
+        text-align: left;
+
+        h1 {
+          font-size: 2.4rem;
+          letter-spacing: .2em;
+        }
+
+        .profile {
+          display: flex;
+          flex-direction: column;
+          height: 40%;
+          justify-content: space-between;
+
+          p {
+            white-space: nowrap;
+
+            span {
+              border-right: 2px solid #ccc ;
+              padding: 0 .4rem;
+              &:first-child {
+                padding-left: 0;
+              }
+              &:last-child {
+                border-right: none;
+              }
+            }
+          }
+        }
+      }
+
+      .avatar-wrapper {
+        align-items: center;
+        border-radius: .5rem;
+        display: flex;
+        height: 10rem;
+        justify-content: center;
+        overflow: hidden;
+        position: absolute;
+        right: 5%;
+        transform: scale(.7) translateX(25%);
+        width: 10rem;
+
+        img.avatar {
+          max-height: 10rem;
+          max-width: 10rem;
+        }
+
+        .default-avatar {
+          background-color: #fff;
+          border-radius: 50%;
+          box-shadow: 0 .25rem .5rem 0 #aaa;
+          height: 90%;
+          position: relative;
+          width: 90%;
+
+          * {
+            opacity: 0;
+          }
+
+          .left-eye, .right-eye {
+            border-radius: 50%;
+            height: 2rem;
+            overflow: hidden;
+            position: absolute;
+            top: 2.5rem;
+            width: 4rem;
+
+            &::after {
+              border: .3rem solid;
+              border-radius: 3rem / 7.5rem;
+              content: '';
+              display: block;
+              height: 25rem;
+              width: calc(100% - .6rem);
+            }
+          }
+
+          .left-eye {
+            left: .75rem;
+          }
+
+          .right-eye {
+            right: .75rem;
+          }
+
+          .mouth {
+            border-radius: 50%;
+            bottom: 1rem;
+            height: 2.5rem;
+            left: 0;
+            margin: 0 auto;
+            overflow: hidden;
+            position: absolute;
+            right: 0;
+            width: 6.5rem;
+
+            &::after {
+              border: .3rem solid;
+              border-radius: 3.5rem / 7.5rem;
+              bottom: 0;
+              content: '';
+              display: block;
+              height: 25rem;
+              left: 0;
+              position: absolute;
+              width: calc(100% - .6rem);
+            }
+          }
+        }
+      }
+    }
+
+    main {
+      
+    }
+
+    footer {
+      background-color: var(--themeColor);
+      line-height: 5rem;
+      padding-top: calc(297mm - 350px - 5rem);
+      text-align: left;
+      text-indent: 2em;
+    }
+  }
+</style>
+
+<!-- <style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style> -->
---
+++ MyResumeDemo_v23/src/components/Block.vue
@@ -0,0 +1,250 @@
+<script setup>
+	
+defineProps({
+	blockValue: {
+		type: Object,
+		default: () => ({})
+	}
+})
+</script>
+
+<template>
+	<div class="block-comp">
+		<div class="block-margin-top"/>
+		<div class="title">{{blockValue.title || 'title'}}</div>
+		<div class="content" :class="`column-${blockValue.columns}`">
+			<div class="option" v-for="(option, index) in blockValue.options" :key="index">
+				<div :class="['key', option['no-key-before'] && 'no-key-before']">
+					{{option.key || 'option.key'}}
+					<p class="tip" v-if="option.tip">({{option.tip}})</p>
+				</div>
+				<div v-if="option.value !== 'null'" class="value">{{option.value || 'option.value'}}</div>
+				<ul v-if="option.details && option.details.length">
+					<li v-for="(detail, index) in option.details" :key="index">{{detail}}</li>
+				</ul>
+			</div>
+		</div>
+		<div class="paperclip">
+			<div class="inner"/>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.block-comp {
+		background-color: #fff;
+		position: relative;
+		&:first-of-type {
+
+		}
+    &:nth-of-type(2), &:nth-of-type(3) {
+      page-break-inside: avoid;
+    }
+    &:nth-of-type(4) {
+    	page-break-before: always;
+    }
+		&:last-of-type {
+			
+		}
+
+		.block-margin-top {
+      background-color: var(--themeColor);
+      height: 4rem;
+      margin-bottom: 2.5rem
+    }
+
+		.title {
+			background-color: #4a4f5a;
+			border-radius: 3rem;
+			border-bottom-left-radius: 0;
+			box-shadow: 3px 2px 6px 0 #8f8a8a;
+			color: #fff;
+			font-size: 1.5rem;
+			font-weight: bold;
+			height: 3rem;
+			left: 5%;
+			line-height: 3rem;
+			overflow: hidden;
+			padding: 0 2.5rem;
+			position: absolute;
+			top: 1.8rem;
+			transform: rotate(-6deg);
+			max-width: 70%;
+		}
+
+		.content {
+			background-color: #fff;
+			display: flex;
+			flex-wrap: wrap;
+			padding: 0 5%;
+			&.column-1 {
+				.option {
+					margin-bottom: 1rem;
+					width: 100%;
+
+					.value {
+						color: #333;
+						font-size: 1.1rem;
+					}
+
+					ul {
+						li {
+							margin-bottom: .5rem;
+						}
+					}
+				}
+			}
+			&.column-3 {
+				.option {
+					margin-bottom: 1rem;
+					width: 33.33%;
+					&:nth-child(2) {
+						text-align: center;
+					}
+					&:nth-child(3) {
+						text-align: right;
+					}
+
+					.key {
+						padding-left: 0;
+						&::before {
+							content: none;
+						}
+
+					}
+
+					.value {
+						color: #333;
+						font-size: 1.1rem;
+					}
+
+					ul {
+						li {
+							margin-bottom: .35rem;
+						}
+					}
+				}
+			}
+
+			.option {
+				page-break-inside: avoid;
+				text-align: left;
+				width: 50%;
+
+				.key {
+					color: #333;
+					font-size: 1rem;
+					line-height: 1.5rem;
+					padding-left: 1.5rem;
+					position: relative;
+					width: 100%;
+					&::before {
+						background-color: #333;
+						border-radius: 50%;
+						content: '';
+						height: .5rem;
+						left: 0;
+						position: absolute;
+						top: .45rem;
+						width: .5rem;
+					}
+					&.no-key-before {
+						padding-left: 0;
+						&::before {
+							content: none;
+						}
+					}
+
+					.tip {
+						display: inline;
+						font-size: .8rem;
+					}
+				}
+
+				.value {
+					color: #666;
+					font-size: 1rem;
+					height: 2.5rem;
+					line-height: 1.5rem;
+					padding-left: 1.5rem;
+				}
+
+				ul {
+					color: #666;
+					list-style: none;
+					padding-left: 1.5rem;
+
+					li {
+						position: relative;
+						text-indent: .8em;
+
+						&::before {
+							background-color: #666;
+							border-radius: 50%;
+							content: '';
+							height: .35rem;
+							left: 0;
+							position: absolute;
+							top: .45rem;
+							width: .35rem;
+						}
+					}
+				}
+			}
+		}
+
+		.paperclip {
+			$color: #000;
+			$innerColor: #888;
+
+			background: linear-gradient(to right, $innerColor, $innerColor) 0 22px/ 25px 3px no-repeat;
+			height: 60px;
+			left: 0;
+			overflow: hidden;
+			position: absolute;
+			top: 2.4rem;
+			transform: rotate(3deg);
+			width: 100px;
+			&::before, .inner {
+				content: '';
+				display: block;
+				height: .5rem;
+				left: 0;
+				position: absolute;
+				top: 50%;
+				transform: translateY(-50%);
+			}
+			&::before {
+				border: 3px solid $color;
+				border-radius: 20px;
+				height: 30px;
+				left: -30px;
+				width: 110px;
+				z-index: 99;
+			}
+			.inner {
+				height: 20px;
+				left: 25px;
+				overflow: hidden;
+				top: 50%;
+				transform: translateY(-50%);
+				width: 70px;
+				z-index: 99;
+					&::before {
+					border: 3px solid $innerColor;
+					border-radius: 20px;
+					content: '';
+					display: block;
+					height: 10px;
+					left: -40px;
+					position: absolute;
+					top: 50%;
+					transform: translateY(-50%);
+					width: 60px;
+					z-index: 99;
+				}
+			}
+		}		
+	}
+</style>
+
